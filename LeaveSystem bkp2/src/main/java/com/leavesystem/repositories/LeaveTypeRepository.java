bdf7c5fb3eb330e09684f0ab98e687b4e35d0db2
nikolayav <Pavkata90_vegana>
2020-01-06 11:29:11 +0200

    Removed duplicate folder from repository

--- LeaveSystem bkp2/src/main/java/com/leavesystem/repositories/LeaveTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leavesystem.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.leavesystem.entity.LeaveType;
-
-public interface LeaveTypeRepository extends CrudRepository<LeaveType, Long> {
-
-}
