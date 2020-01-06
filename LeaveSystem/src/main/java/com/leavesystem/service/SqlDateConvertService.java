98672097edcfd613040d8c266c53b6407e475fd8
nikolayav <Pavkata90_vegana>
2020-01-06 11:44:10 +0200

    Added java.sql.Date convert class.

---
+++ LeaveSystem/src/main/java/com/leavesystem/service/SqlDateConvertService.java
@@ -0,0 +1,57 @@
+package com.leavesystem.service;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leavesystem.entity.Request;
+import com.leavesystem.repositories.LeaveTypeRepository;
+import com.leavesystem.repositories.RequestRepository;
+import com.leavesystem.repositories.UserRepository;
+
+@Service
+public class SqlDateConvertService {
+	
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
+	private RequestRepository requestRepo;
+
+	@Autowired
+	private LeaveTypeRepository leaveTypeRepo;
+	
+	public int calculateTotalDaysOfLeaveReturn(Request request) {
+		
+		Date dateStart = (Date) request.getDateFrom();
+		Date dateEnd = (Date) request.getDateTo();
+		
+		LocalDate localDateStart = dateStart.toLocalDate();
+		
+		LocalDate localDateTo = dateEnd.toLocalDate();
+		
+		
+		Period p = Period.between(localDateStart, localDateTo) ;
+		int leaveDays = p.getDays() + 1;
+		
+		int totalDays = leaveDays;
+		
+		for (int i = 0; i < leaveDays; i++) {
+			LocalDate date = localDateStart.plusDays(i);
+			switch (date.getDayOfWeek()) {
+			case SATURDAY:
+			case SUNDAY:
+				totalDays -= 1;
+				break;
+			}
+		}
+
+		return totalDays;
+	}
+	
+}
+
+
